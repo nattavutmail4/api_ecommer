c979deb4304b6410c2a944d5fe4799a7d56a25c9
AdminOffice <nattavutmail3@gmail.com>
2023-01-30 13:45:54 +0700

    Update_routes

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+
 const {
     getaUser,
     getallUser,
@@ -11,12 +12,15 @@ const {
     blockUser
 } = require('../controller/userCtrl')
 
+
 const  {authMiddleware , isAdmin}  = require('../middlewares/authMiddleware')
 
 router.get('/all-users',getallUser)
 router.get('/:id', authMiddleware, isAdmin, getaUser)
 
-router.post('/register', createUser)
+router.post('/register',createUser)
+
+
 router.post('/login', loginUserCtrl)
 
 router.put('/:id',authMiddleware, updatedUser)
